3000f7e0fd96400a36793d7e7fad514c633d05a2
Manhhuy24 <huynhmanhhuypyn@gmail.com>
2024-11-18 20:08:46 +0700

    up

---
+++ src/views/Pages/DanhSachLop/DanhSachLop.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import './DanhSachLop.css'
+
+const DanhSachLop = () => {
+    const [showAddStudentModel, setShowAddStudentModel] = useState(false);
+
+    const handleAddStudent = () => {
+        setShowAddStudentModel(true);
+    };
+
+    const closeAddStudentModel = () => {
+        setShowAddStudentModel(false);
+    };
+
+    return (
+        <div>
+            <h1>Lập danh sách lớp</h1>
+            <div className="select-group">
+                <select name="year">
+                    <option value="2022-2023">2022-2023</option>
+                    <option value="2023-2024">2023-2024</option>
+                    <option value="2024-2025">2024-2025</option>
+                </select>
+                <select name="classes">
+                    <option value="10A1">10A1</option>
+                    <option value="10A2">10A2</option>
+                    <option value="10A3">10A3</option>
+                </select>
+                <input type="text" name="number" value="40" readonly />
+            </div>
+
+            <div className="button-group">
+                <button className="btn btn-primary" onClick={handleAddStudent}>
+                    <i className="bx bx-plus"></i> Thêm học sinh vào danh sách
+                </button>
+            </div>
+
+            <div className="table-responsive">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th className="text-center">STT</th>
+                            <th className="text-center">Họ và tên</th>
+                            <th className="text-center">Giới tính</th>
+                            <th className="text-center">Năm sinh</th>
+                            <th className="text-center">Địa chỉ</th>
+                            <th className="text-center">Email</th>
+                            <th className="text-center">Xoá</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="text-center">1</td>
+                            <td className="text-center">Nguyễn A</td>
+                            <td className="text-center">Nam</td>
+                            <td className="text-center">11-11-2008</td>
+                            <td className="text-center">abc</td>
+                            <td className="text-center">123@gmail</td>
+                            <td className="text-center">
+                                <button className="btn btn-delete"><i className="bx bx-trash"></i></button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="text-center">2</td>
+                            <td className="text-center">Trần B</td>
+                            <td className="text-center">Nữ</td>
+                            <td className="text-center">02-02-2008</td>
+                            <td className="text-center">def</td>
+                            <td className="text-center">456@gmail</td>
+                            <td className="text-center">
+                                <button className="btn btn-delete"><i className="bx bx-trash"></i></button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            {
+                showAddStudentModel && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close" onClick={closeAddStudentModel}>&times;</span>
+                            <h2>Thêm lớp học</h2>
+                            <form>
+                                <div>
+                                    <label>Họ và tên:</label>
+                                    <input type="text" name="name" placeholder="Nguyễn A" />
+                                </div>
+                                <div>
+                                    <label>Giới tính:</label>
+                                    <select name="gender">
+                                        <option value="Nam">Nam</option>
+                                        <option value="Nu">Nữ</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Năm sinh:</label>
+                                    <input type="date" name="date" />
+                                </div>
+                                <div>
+                                    <label>Địa chỉ:</label>
+                                    <input type="text" name="address" placeholder="abc" />
+                                </div>
+                                <div>
+                                    <label>Email:</label>
+                                    <input type="text" name="email" placeholder="123@gmail" />
+                                </div>
+                                <button type="submit">Lưu</button>
+                            </form>
+                        </div>
+                    </div>
+                )
+            }
+
+        </div>
+    );
+};
+
+export default DanhSachLop;
--- src/views/Pages/QuanLyLopHoc/QuanLyLopHoc.js
+++ src/views/Pages/QuanLyLopHoc/QuanLyLopHoc.js
@@ -1,11 +1,135 @@
-import React from 'react';
-import './QuanLyLopHoc.css'
+import React, { useState } from 'react';
+import './QuanLyLopHoc.css';
 
 const QuanLyLopHoc = () => {
+    const [showAddClassModel, setShowAddClassModel] = useState(false);
+    const [showEditClassModel, setShowEditClassModel] = useState(false);
+
+    const handleAddClass = () => {
+        setShowAddClassModel(true);
+    };
+
+    const closeAddClassModel = () => {
+        setShowAddClassModel(false);
+    };
+
+
+    const handleEditClass = () => {
+        setShowEditClassModel(true);
+    };
+
+    const closeEditClassModel = () => {
+        setShowEditClassModel(false);
+    };
+
     return (
-        <div>
-            <h1>Quản lý lớp học</h1>
-            {/* Nội dung của trang Danh sách học sinh */}
+        <div className="container">
+            <h1 className="h1.tiepnhan">Quản lý lớp học</h1>
+            <div className="button-group">
+                <button className="btn btn-primary" onClick={handleAddClass}>
+                    <i className="bx bx-plus"></i> Thêm lớp học mới
+                </button>
+            </div>
+
+            <div className="table-responsive">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th className="text-center">STT</th>
+                            <th className="text-center">Tên lớp học</th>
+                            <th className="text-center">Khối lớp</th>
+                            <th className="text-center">Chỉnh sửa</th>
+                            <th className="text-center">Xoá</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="text-center">1</td>
+                            <td className="text-center">Lớp 10A1</td>
+                            <td className="text-center">Khối 10</td>
+                            <td className="text-center">
+                                <button className="btn btn-edit" onClick={() => handleEditClass()}>
+                                    <i className="bx bxs-edit"></i>
+                                </button>
+                            </td>
+                            <td className="text-center">
+                                <button className="btn btn-delete"><i className="bx bx-trash"></i></button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="text-center">2</td>
+                            <td className="text-center">Lớp 10A2</td>
+                            <td className="text-center">Khối 10</td>
+                            <td className="text-center">
+                                <button className="btn btn-edit" onClick={() => handleEditClass()}>
+                                    <i className="bx bxs-edit"></i>
+                                </button>
+                            </td>
+
+                            <td className="text-center">
+                                <button className="btn btn-delete"><i className="bx bx-trash"></i></button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            {
+                showAddClassModel && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close" onClick={closeAddClassModel}>&times;</span>
+                            <h2>Thêm lớp học</h2>
+                            <form>
+                                <div>
+                                    <label>Tên lớp học:</label>
+                                    <input type="text" name="classname" placeholder="Lớp 10A1" />
+                                </div>
+                                <div>
+                                    <label>Tên khối:</label>
+                                    <select name="grade">
+                                        <option value="Khối 10">Khối 10</option>
+                                        <option value="Khối 11">Khối 11</option>
+                                        <option value="Khối 12">Khối 12</option>
+                                    </select>
+                                </div>
+                                <button type="submit">Lưu</button>
+                            </form>
+                        </div>
+                    </div>
+                )
+            }
+
+            {
+                showEditClassModel && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <span className="close" onClick={closeEditClassModel}>&times;</span>
+                            <h2>Chỉnh sửa lớp học</h2>
+                            <form>
+                                <div>
+                                    <label>Mã lớp học:</label>
+                                    <input type="text" name="id" value="1" readonly />
+                                </div>
+                                <div>
+                                    <label>Tên lớp học:</label>
+                                    <input type="text" name="classname" />
+                                </div>
+                                <div>
+                                    <label>Tên khối:</label>
+                                    <select name="grade">
+                                        <option value="Khối 10">Khối 10</option>
+                                        <option value="Khối 11">Khối 11</option>
+                                        <option value="Khối 12">Khối 12</option>
+                                    </select>
+                                </div>
+                                <button type="submit">Lưu</button>
+                            </form>
+                        </div>
+                    </div>
+                )
+            }
+
         </div>
     );
 };
